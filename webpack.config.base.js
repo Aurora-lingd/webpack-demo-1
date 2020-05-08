e9f030e2cf05b7f4200fdceaca3edbe3f8d8df11
tracy <843358687@qq.com>
2020-05-08 22:02:00 +0800

    updata

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,5 +12,41 @@ module.exports = {
             title: 'My App',
             template: 'src/assets/index.html'
         }),
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    //把文件变成文件路径
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.styl$/,
+                loader: ['style-loader', 'css-loader', 'stylus-loader']
+            },
+            {
+                test: /\.less$/,
+                loader: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    //转化成style标签
+                    'style-loader',
+                    // 转化成js字符串
+                    'css-loader',
+                    {
+                        //sass-loader 转化css 代码
+                        loader:
+                            "sass-loader",
+                        options: {
+                            implementation: require('dart-sass')
+                        }
+                    }
+                ],
+            },
+        ],
+    },
 };
